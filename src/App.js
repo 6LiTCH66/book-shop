703dba7ffbc097e183ca8d223f3dc7eb5dd88b5e
Game <ilja200303@gmail.com>
2021-05-05 18:11:54 +0300

    fix gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import {DataProvider} from "./components/Context";
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import Products from './section/Products';
 import Details from "./section/Details";
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 class Section extends Component {
     render() {
         return (
             <section>
-                <Route path="/product" component={Products} exact/>
-                <Route path="/product/:id" component={Details}/>
+                <Switch>
+                    <Route path="/product" component={Products} exact/>
+                    <Route path="/product/:id" component={Details}/>
+                </Switch>
             </section>
         );
     }
